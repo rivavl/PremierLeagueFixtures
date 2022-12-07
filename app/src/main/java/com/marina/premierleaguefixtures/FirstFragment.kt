e301c327cbd1651f1c1e0831bd43ff9032536580
rivavl <mvkalinenkova@yandex.ru>
2022-12-07 10:19:49 +0300

    2 HW done

--- app/src/main/java/com/marina/premierleaguefixtures/FirstFragment.kt
+++ app/src/main/java/com/marina/premierleaguefixtures/FirstFragment.kt
@@ -1,5 +1,9 @@
 package com.marina.premierleaguefixtures
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.marina.premierleaguefixtures.databinding.FragmentFirstBinding
 
@@ -8,6 +12,16 @@ class FirstFragment : Fragment(R.layout.fragment_first) {
     private var _binding: FragmentFirstBinding? = null
     private val binding: FragmentFirstBinding
         get() = _binding ?: throw RuntimeException("FragmentFirstBinding == null")
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onDestroy() {
         super.onDestroy()
--- app/src/main/java/com/marina/premierleaguefixtures/SecondFragment.kt
+++ app/src/main/java/com/marina/premierleaguefixtures/SecondFragment.kt
@@ -1,5 +1,9 @@
 package com.marina.premierleaguefixtures
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.marina.premierleaguefixtures.databinding.FragmentSecondBinding
 
@@ -8,6 +12,16 @@ class SecondFragment : Fragment(R.layout.fragment_second) {
     private var _binding: FragmentSecondBinding? = null
     private val binding: FragmentSecondBinding
         get() = _binding ?: throw RuntimeException("FragmentSecondBinding == null")
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onDestroy() {
         super.onDestroy()
