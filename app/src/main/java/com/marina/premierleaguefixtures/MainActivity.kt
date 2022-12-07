fafaeca2ef638b9e3b0f23d25e89b43074028e14
rivavl <mvkalinenkova@yandex.ru>
2022-12-07 09:49:38 +0300

    Initial commit

---
+++ app/src/main/java/com/marina/premierleaguefixtures/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.marina.premierleaguefixtures
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
