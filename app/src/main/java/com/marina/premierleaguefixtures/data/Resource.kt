f31ec12e194a7e9ee3a1a514bd7ab574f99a3d14
rivavl <mvkalinenkova@yandex.ru>
2022-12-19 13:16:58 +0300

    5 HW done (Coroutines)

---
+++ app/src/main/java/com/marina/premierleaguefixtures/data/Resource.kt
@@ -0,0 +1,7 @@
+package com.marina.premierleaguefixtures.data
+
+sealed class Resource<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String? = null, data: T? = null) : Resource<T>(data, message)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+}
--- app/src/main/java/com/marina/premierleaguefixtures/presentation/list/ListFragmentViewModel.kt
+++ app/src/main/java/com/marina/premierleaguefixtures/presentation/list/ListFragmentViewModel.kt
@@ -4,7 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.marina.premierleaguefixtures.data.mapper.toUI
+import com.marina.premierleaguefixtures.data.Resource
+import com.marina.premierleaguefixtures.data.remote.NetworkHelper
 import com.marina.premierleaguefixtures.data.remote.RetrofitInstance
 import com.marina.premierleaguefixtures.model.Match
 import kotlinx.coroutines.launch
@@ -13,9 +14,10 @@ class ListFragmentViewModel : ViewModel() {
 
     // пока так, когда добавлю di, исправлю
     private val api = RetrofitInstance.matchesApi
+    private val apiHelper = NetworkHelper(api)
 
-    private val _matchesList = MutableLiveData<List<Match>>()
-    val matchesList: LiveData<List<Match>> get() = _matchesList
+    private val _matchesList = MutableLiveData<Resource<List<Match>>>()
+    val matchesList: LiveData<Resource<List<Match>>> get() = _matchesList
 
     private val _isLoading = MutableLiveData<Boolean>()
     val isLoading: LiveData<Boolean> get() = _isLoading
@@ -25,13 +27,20 @@ class ListFragmentViewModel : ViewModel() {
     }
 
     private fun loadMatches() = viewModelScope.launch {
-        setLoading(true)
-        val list = api.getMatchesList().toUI()
-        _matchesList.value = list
-        setLoading(false)
-    }
-
-    private fun setLoading(isLoading: Boolean) {
-        _isLoading.value = isLoading
+        apiHelper.getMatches().collect { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _matchesList.postValue(
+                        Resource.Success(result.data!!)
+                    )
+                }
+                is Resource.Loading -> {
+                    _matchesList.postValue(Resource.Loading())
+                }
+                is Resource.Error -> {
+                    _matchesList.postValue(Resource.Error())
+                }
+            }
+        }
     }
 }
